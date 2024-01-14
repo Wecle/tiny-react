cd906fbafdd71e91ebe50840f726fba9e35918e5
Wecle <837968732@qq.com>
2024-01-14 16:49:55 +0800

    feat: support JSX

---
+++ vite-runner/App.jsx
@@ -0,0 +1,19 @@
+import React from './core/React.js'
+
+const App = (
+    <div id="app">
+        Hello World
+        <div style="color: red">Hello World</div>
+    </div>
+)
+
+// const App = () => {
+//     return (
+//         <div id="app">
+//             Hello World
+//             <div style="color: red">Hello World</div>
+//         </div>
+//     )
+// }
+
+export default App
